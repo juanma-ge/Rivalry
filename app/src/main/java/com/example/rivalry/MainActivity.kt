22bb8cb69bfe56b6739e481b8b7ea6f756226659
Juanma <juanmagomezestudillo@gmail.com>
2026-02-19 09:42:09 +0100

    Integración de Firebase.

--- app/src/main/java/com/example/rivalry/MainActivity.kt
+++ app/src/main/java/com/example/rivalry/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.rivalry
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,36 +13,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.rivalry.ui.theme.RivalryTheme
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            RivalryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Text("¡Hola, el código de Firebase se está ejecutando!")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RivalryTheme {
-        Greeting("Android")
     }
 }
